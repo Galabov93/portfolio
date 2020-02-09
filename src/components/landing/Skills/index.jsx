df14ea8ce728edd831641467ef9d8bd906002402
Yordan Galabov <galabovj@gmail.com>
2020-02-09 18:05:41 +0200

    Add intro messages

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { Container, Button } from 'components/common'
-import dev from 'assets/illustrations/skills.svg'
-import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles'
+import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { Container, Button } from 'components/common';
+import dev from 'assets/illustrations/skills.svg';
+import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
 
 export const Skills = () => (
   <Wrapper id="about">
     <SkillsWrapper as={Container}>
       <Thumbnail>
-        <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+        <img src={dev} alt="I am Danny and I'm a Full Stack Web developer" />
       </Thumbnail>
       <Details>
-        <h1>Hi There!</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry’s standard dummy.
-        </p>
+        <h1>Skills</h1>
+        <p>I am a Full Stack Developer experienced working with Node.js and React/GatsbyJS/NextJS</p>
         <Button as={AnchorLink} href="#contact">
           Hire me
         </Button>
       </Details>
     </SkillsWrapper>
   </Wrapper>
-)
+);
